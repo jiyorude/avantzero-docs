dc9e6543b2b1ff4161f7ef2dbc281e8b3017c5aa
Jordy Veenstra <jordy.gaptx@gmail.com>
2025-08-15 15:46:55 +0200

    15/08 15:46
    a11y button fixes: now entire button can be clicked, logo fix: outline svg logo used, appov logo fix: replaced png with proper svg, footer: responsivity fixes

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-import Logo from '@/public/images/APPOV_WHITE.png';
+import APPOVLogo from '@/public/images/APPOV_Logo.svg';
+
 import Link from 'next/link';
 
 const Footer: React.FC = () => {
@@ -8,25 +9,26 @@ const Footer: React.FC = () => {
     const year = date.getFullYear();
 
     return (
-        <footer className='bottom-0 fixed flex flex-col shadow-xl select-none'>
+        <footer className='bottom-0 fixed flex md:flex-col shadow-xl select-none'>
             <section className='w-16 bg-AVZBla h-14 mxFlexCenter' aria-label='Container containing the A Pixelated Point of View Logo'>
                 <Image
-                    src={Logo}
+                    src={APPOVLogo}
                     alt="A Pixelated Point of View Logo"
                     priority
                     width={37}
+                    height={37}
                     className='pt-[2px]'
+                    quality={100}
                 />
             </section>
-            <section className='w-16 bg-AVZTea h-14 mxFlexCenter' aria-label='Container containing a link for further information regarding the MIT-License'>
-                <Link href='https://opensource.org/license/mit' target='_blank'>
+            <button className='w-16 bg-AVZTea h-14' aria-label='Container containing a link for further information regarding the MIT-License'>
+                <Link href='https://opensource.org/license/mit' target='_blank' className='mxFlexCenter w-full h-full'>
                     <h3 className='text-AVZWhi opacity-90 font-AVZBla text-md'>MIT</h3>
                 </Link>
-            </section>
-            <section className='w-16 bg-AVZRed h-14 mxFlexCenter flex-col leading-3.5' aria-label='Container with copyright and documentation version information.'>
+            </button>
+            <section className='w-16 bg-AVZRed h-14 mxFlexCenter flex-col leading-3.5 ' aria-label='Container with copyright and documentation version information.'>
                 <h3 className='text-AVZWhi opacity-50 font-AVZBol text-[12px]'>0.1.0</h3>
                 <p className='text-AVZWhi opacity-50 font-AVZLight text-[10px]'>&copy; {year}</p>
-
             </section>
         </footer>
     )
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { IoLogoGithub, IoLogoPython } from 'react-icons/io';
 import { MdDownload } from 'react-icons/md';
+import AvantZeroLogo from '@/public/images/AvantZero_Logo.svg';
 
 const Header: React.FC = () => {
     return (
         <header className='h-20 flex md:justify-end select-none'>
             <section className='shadow-xl flex md:justify-end w-full md:w-fit'>
-                <button className='bg-AVZWhi w-1/5 md:w-28 h-full mxFlexCenter'>
-                    <Link href='https://github.com/jiyorude/avantzero/releases' target='_blank'>
+                <button className='bg-AVZWhi w-1/5 md:w-28 h-full hover:cursor-pointer'>
+                    <Link href='https://github.com/jiyorude/avantzero/releases' target='_blank' className='mxFlexCenter h-full w-full'>
                         <MdDownload size={45} className='fill-AVZGre bg-transparent size-10' title='Download the latest release' />
                     </Link>
                 </button>
-                <button className='bg-AVZRed w-1/5 md:w-28 h-full mxFlexCenter'>
-                    <Link href='https://pypi.org/project/avantzero/' target='_blank'>
+                <button className='bg-AVZRed w-1/5 md:w-28 h-full hover:cursor-pointer'>
+                    <Link href='https://pypi.org/project/avantzero/' target='_blank' className='mxFlexCenter h-full w-full'>
                         <IoLogoPython size={45} className='fill-AVZBla bg-transparent size-10' title='Check out the PyPi project page' />
                     </Link>
                 </button>
-                <button className='bg-AVZTea w-1/5 md:w-28 h-full mxFlexCenter'>
-                    <Link href='https://github.com/jiyorude/avantzero/' target='_blank'>
+                <button className='bg-AVZTea w-1/5 md:w-28 h-full hover:cursor-pointer'>
+                    <Link href='https://github.com/jiyorude/avantzero/' target='_blank' className='mxFlexCenter h-full w-full'>
                         <IoLogoGithub size={45} className='fill-AVZWhi bg-transparent size-10' title='Visit the GitHub repository' />
                     </Link>
                 </button>
-                <button className='bg-AVZBla w-2/5 md:w-48 h-full mxFlexCenter'>
-                    <Link href="/" >
-                       <h1 className='font-AVZMai text-AVZWhi text-[65px] mb-5'>A</h1>
+                <button className='bg-AVZBla relative w-2/5 md:w-48 h-full hover:cursor-pointer z-10'>
+                    <Link href="/" className='mxFlexCenter h-full w-full'>
+                       <Image 
+                            src={AvantZeroLogo}
+                            alt="AvantZero Logo"
+                            width={65}
+                            height={65}
+                            priority
+                            quality={100}
+                       />
                     </Link>
                 </button>
             </section>
